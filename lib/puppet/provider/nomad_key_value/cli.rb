0f516dfe0efd993d658dc11acdb9628cc8970bde
Massimiliano Adamo <maxadamo@gmail.com>
2024-11-12 19:05:57 +0100

    try using update method

--- lib/puppet/provider/nomad_key_value/cli.rb
+++ lib/puppet/provider/nomad_key_value/cli.rb
@@ -1,4 +1,5 @@
 require 'json'
+require 'tempfile'
 
 Puppet::Type.type(:nomad_key_value).provide(:cli) do
   desc 'Provider to manage Nomad variables using `nomad var put`.'
@@ -8,22 +9,20 @@ Puppet::Type.type(:nomad_key_value).provide(:cli) do
   end
 
   def build_command_args
-    unless File.executable?(resource[:binary_path])
-      raise Puppet::Error, "Nomad binary at #{resource[:binary_path]} is not executable or not found."
-    end
+    raise Puppet::Error, "Nomad binary at #{resource[:binary_path]} is not executable or not found." unless File.executable?(resource[:binary_path])
 
     args = []
-    args << "-token=#{resource[:token]}" unless resource[:token].nil? || resource[:token].empty?
+    args << "-token=#{resource[:token]}" unless resource[:token].to_s.empty?
     args << "-address=#{resource[:address]}"
-    args << "-datacenter=#{resource[:datacenter]}" unless resource[:datacenter].nil? || resource[:datacenter].empty?
+    args << "-datacenter=#{resource[:datacenter]}" unless resource[:datacenter].to_s.empty?
     args << "-region=#{resource[:region]}"
-    args << "-namespace=#{resource[:namespace]}" unless resource[:namespace].nil? || resource[:namespace].empty?
-    args << "-tls-server-name=#{resource[:tls_server_name]}" unless resource[:tls_server_name].nil? || resource[:tls_server_name].empty?
+    args << "-namespace=#{resource[:namespace]}" unless resource[:namespace].to_s.empty?
+    args << "-tls-server-name=#{resource[:tls_server_name]}" unless resource[:tls_server_name].to_s.empty?
     args << '-tls-skip-verify' if resource[:skip_verify] == true
-    args << "-client-key=#{resource[:client_key]}" unless resource[:client_key].nil? || resource[:client_key].empty?
-    args << "-client-cert=#{resource[:client_cert]}" unless resource[:client_cert].nil? || resource[:client_cert].empty?
-    args << "-ca-cert=#{resource[:ca_cert]}" unless resource[:ca_cert].nil? || resource[:ca_cert].empty?
-    args << "-ca-path=#{resource[:ca_path]}" unless resource[:ca_path].nil? || resource[:ca_path].empty?
+    args << "-client-key=#{resource[:client_key]}" unless resource[:client_key].to_s.empty?
+    args << "-client-cert=#{resource[:client_cert]}" unless resource[:client_cert].to_s.empty?
+    args << "-ca-cert=#{resource[:ca_cert]}" unless resource[:ca_cert].to_s.empty?
+    args << "-ca-path=#{resource[:ca_path]}" unless resource[:ca_path].to_s.empty?
     args
   end
 
@@ -42,29 +41,33 @@ Puppet::Type.type(:nomad_key_value).provide(:cli) do
     @modify_index = result['ModifyIndex']
     @existing_items = result['Items']
 
-    @existing_items == resource[:value] # Return true if Items matches the desired value
+    @existing_items == resource[:value]
   end
 
   def create
-    json_value = { 'Items' => resource[:value] }.to_json
-    command = [nomad_command, 'var', 'put', '-in', 'json'] + build_command_args
+    run_nomad_command(resource[:value])
+  end
 
-    if @existing_items && @existing_items != resource[:value]
-      # Key exists but has different values; use ModifyIndex for an update
-      command += ['-check-index', @modify_index.to_s]
-    end
-
-    command += [resource[:name], '-']
-    Tempfile.open('nomad_var') do |tempfile|
-      tempfile.write(json_value)
-      tempfile.flush
-
-      execute(command, stdinfile: tempfile.path)
-    end
+  def update
+    run_nomad_command(resource[:value], @modify_index)
   end
 
   def destroy
     command = [nomad_command, 'var', 'delete'] + build_command_args + [resource[:name]]
     execute(command)
+  end
+
+  private
+
+  def run_nomad_command(value, modify_index = nil)
+    json_value = { 'Items' => value }.to_json
+    command = [nomad_command, 'var', 'put', '-in', 'json'] + build_command_args
+    command += ['-check-index', modify_index.to_s] if modify_index
+
+    Tempfile.open('nomad_var') do |tempfile|
+      tempfile.write(json_value)
+      tempfile.flush
+      execute(command + [resource[:name], '-'], stdinfile: tempfile.path)
+    end
   end
 end
