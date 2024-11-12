1a0c6a0d04b4257b4b2780bf45ba567dae39fd98
Massimiliano Adamo <maxadamo@gmail.com>
2024-11-12 13:33:21 +0100

    start testing the provider

---
+++ lib/puppet/provider/nomad_key_value/cli.rb
@@ -0,0 +1,63 @@
+require 'json'
+
+Puppet::Type.type(:nomad_key_value).provide(:cli) do
+  desc 'Provider to manage Nomad variables using `nomad var put`.'
+
+  confine :true => File.executable?(resource[:binary_path])
+
+  def nomad_command
+    resource[:binary_path]
+  end
+
+  def build_command_args
+    args = []
+    args << "-token=#{resource[:nomad_token]}" unless resource[:nomad_token].nil? || resource[:nomad_token].empty?
+    args << "-address=#{resource[:nomad_url]}"
+    args << "-datacenter=#{resource[:datacenter]}" unless resource[:datacenter].nil? || resource[:datacenter].empty?
+    args << "-region=#{resource[:region]}"
+    args << "-namespace=#{resource[:namespace]}" unless resource[:namespace].nil? || resource[:namespace].empty?
+    args << "-tls-server-name=#{resource[:tls_server_name]}" unless resource[:tls_server_name].nil? || resource[:tls_server_name].empty?
+    args << '-tls-skip-verify' if resource[:skip_verify] == true
+    args << "-client-key=#{resource[:client_key]}" unless resource[:client_key].nil? || resource[:client_key].empty?
+    args << "-client-cert=#{resource[:client_cert]}" unless resource[:client_cert].nil? || resource[:client_cert].empty?
+    args << "-ca-cert=#{resource[:ca_cert]}" unless resource[:ca_cert].nil? || resource[:ca_cert].empty?
+    args << "-ca-path=#{resource[:ca_path]}" unless resource[:ca_path].nil? || resource[:ca_path].empty?
+    args
+  end
+
+  def fetch_existing
+    command = [nomad_command, 'var', 'get', '-out', 'json'] + build_command_args + [resource[:name]]
+    output = execute(command, failonfail: false)
+    JSON.parse(output)
+  rescue JSON::ParserError, Puppet::ExecutionFailure
+    nil
+  end
+
+  def exists?
+    result = fetch_existing
+    return false if result.nil?
+
+    @modify_index = result['ModifyIndex']
+    @existing_items = result['Items']
+
+    @existing_items == resource[:value] # Return true if Items matches the desired value
+  end
+
+  def create
+    json_value = { 'Items' => resource[:value] }.to_json
+    command = [nomad_command, 'var', 'put', '-in', 'json'] + build_command_args
+
+    if @existing_items && @existing_items != resource[:value]
+      # Key exists but has different values; use ModifyIndex for an update
+      command += ['-check-index', @modify_index.to_s]
+    end
+
+    command += [resource[:name], '-']
+    execute(command, stdinfile: StringIO.new(json_value))
+  end
+
+  def destroy
+    command = [nomad_command, 'var', 'delete'] + build_command_args + [resource[:name]]
+    execute(command)
+  end
+end
